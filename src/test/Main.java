0541b9c00cd4a7adc0fb85ecc50f7f1b68510696
Illia Ponomarov <chippaxyucac@gmail.com>
2021-10-21 04:06:52 -0400

    change main file

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,7 +1,21 @@
 package test;
 
+
+import java.util.*;
+
 public class Main {
  public static void main(String[] args) {
 	 System.out.println("Hello World");
+	 
+	 ArrayList<String> lol = new ArrayList<String>();
+	 
+	 
+	 lol.add("Illia");
+	 lol.add("Alex");
+	 
+	 for(int i = 0; i < lol.size(); i++)
+		 System.out.println(lol.get(i));
+	 
+	 
  }
 }
---
+++ src/test/persons/Person.java
@@ -0,0 +1,5 @@
+package test.persons;
+
+class Person {
+
+}
